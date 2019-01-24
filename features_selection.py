efc8f6b5451b816b0efb25680207c13c4c01b1a4
Emin Mammadov <emin.ht@gmail.com>
2019-01-24 12:17:09 -0400

    Cleared feature selection process
    Cleared features selection. Corrected the errr with choosing the features I need, thus improving recall to 0.97 (no-fault vs. all-faults case).

--- features_selection.py
+++ features_selection.py
@@ -3,28 +3,39 @@ import numpy as np
 from sklearn import preprocessing as prep
 
 
-def feature_selection (scada_data, df, features):
+def feature_selection (df, features):
         
     dataset_fs = pd.DataFrame(columns = df.columns)    
     droplist = [i for i in df.columns if i not in features]
-    dataset_fs = df.drop(droplist, axis = 1, inplace = False) #if I put [] around droplist, I get an error
-    dataset_fs['Inverter avg.'] = df[['CS101 : Blade B temp.', 'CS101 : Blade C temp.', 'RTU: ava. Setpoint 1', 'CS101 : Sys 1 inverter 1 cabinet temp.',
-                                 'CS101 : Sys 1 inverter 2 cabinet temp.', 'CS101 : Sys 1 inverter 3 cabinet temp.', 'CS101 : Sys 1 inverter 4 cabinet temp.',
-                                 'CS101 : Sys 1 inverter 5 cabinet temp.', 'CS101 : Sys 1 inverter 6 cabinet temp.', 'CS101 : Sys 1 inverter 7 cabinet temp.',
-                                 'CS101 : Sys 2 inverter 1 cabinet temp.', 'CS101 : Sys 2 inverter 2 cabinet temp.',  'CS101 : Sys 2 inverter 3 cabinet temp.',
-                                 'CS101 : Sys 2 inverter 4 cabinet temp.', 'CS101 : Sys 2 inverter 5 cabinet temp.', 'CS101 : Sys 2 inverter 6 cabinet temp.',
-                                 'CS101 : Sys 2 inverter 7 cabinet temp.']].mean(axis=1)
-    dataset_fs['Inverter std'] = df[['CS101 : Blade B temp.', 'CS101 : Blade C temp.', 'RTU: ava. Setpoint 1', 'CS101 : Sys 1 inverter 1 cabinet temp.',
-                                 'CS101 : Sys 1 inverter 2 cabinet temp.', 'CS101 : Sys 1 inverter 3 cabinet temp.', 'CS101 : Sys 1 inverter 4 cabinet temp.',
-                                 'CS101 : Sys 1 inverter 5 cabinet temp.', 'CS101 : Sys 1 inverter 6 cabinet temp.', 'CS101 : Sys 1 inverter 7 cabinet temp.',
-                                 'CS101 : Sys 2 inverter 1 cabinet temp.', 'CS101 : Sys 2 inverter 2 cabinet temp.',  'CS101 : Sys 2 inverter 3 cabinet temp.',
-                                 'CS101 : Sys 2 inverter 4 cabinet temp.', 'CS101 : Sys 2 inverter 5 cabinet temp.', 'CS101 : Sys 2 inverter 6 cabinet temp.',
-                                 'CS101 : Sys 2 inverter 7 cabinet temp.']].std(axis=1)
+    dataset_fs = df.drop(droplist, axis = 1, inplace = False) 
+    dataset_fs['Inverter avg.'] = df[[
+            'CS101 : Sys 1 inverter 1 cabinet temp.',
+            'CS101 : Sys 1 inverter 2 cabinet temp.', 
+            'CS101 : Sys 1 inverter 3 cabinet temp.', 
+            'CS101 : Sys 1 inverter 4 cabinet temp.', 
+            'CS101 : Sys 1 inverter 5 cabinet temp.', 
+            'CS101 : Sys 1 inverter 6 cabinet temp.', 
+            'CS101 : Sys 1 inverter 7 cabinet temp.',
+            'CS101 : Sys 2 inverter 1 cabinet temp.', 
+            'CS101 : Sys 2 inverter 2 cabinet temp.',  
+            'CS101 : Sys 2 inverter 3 cabinet temp.',
+            'CS101 : Sys 2 inverter 4 cabinet temp.']].mean(axis=1)
+    dataset_fs['Inverter std'] = df[[
+            'CS101 : Sys 1 inverter 1 cabinet temp.',
+            'CS101 : Sys 1 inverter 2 cabinet temp.', 
+            'CS101 : Sys 1 inverter 3 cabinet temp.', 
+            'CS101 : Sys 1 inverter 4 cabinet temp.', 
+            'CS101 : Sys 1 inverter 5 cabinet temp.', 
+            'CS101 : Sys 1 inverter 6 cabinet temp.', 
+            'CS101 : Sys 1 inverter 7 cabinet temp.',
+            'CS101 : Sys 2 inverter 1 cabinet temp.', 
+            'CS101 : Sys 2 inverter 2 cabinet temp.',  
+            'CS101 : Sys 2 inverter 3 cabinet temp.',
+            'CS101 : Sys 2 inverter 4 cabinet temp.']].std(axis=1)
     
     # Normalize the data
     temp = dataset_fs.values
-    min_max_scaler = prep.MinMaxScaler()
-    x_scaled = min_max_scaler.fit_transform(temp)
+    x_scaled = prep.normalize(temp)
     normalized_dataset_fs = pd.DataFrame(x_scaled, columns = dataset_fs.columns)
     
     return normalized_dataset_fs
